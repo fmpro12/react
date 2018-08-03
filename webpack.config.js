5a5d140e616e34ec5fc8477eb1ed74a4c85c50c2
Yan <yan@example.com>
2018-08-02 23:53:58 -0700

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,52 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './index.js',
-    output: { path: __dirname, filename: 'bundle.js'},
+
+    entry: "./src/index.js",
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle-[hash].js",
+        publicPath: "/"
+    },
+
     module: {
         rules: [
             {
-                test: /.jsx?$/,
-                loader: 'babel-loader',
+                test: /\.js$/,
                 exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react']
-                }
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+
+            {
+                test: /\.jpg$/,
+                use: {
+                    loader: "file-loader",
+                    query: {
+                        name: "[name]-[hash].[ext]"
+                    }
+                },
             }
         ]
     },
+
     devtool: "source-map",
-	mode: "development",	
+    mode: "development",
+
+devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    proxy: {
+        "/api": "http://localhost:9090"
+    }
+}
 };
