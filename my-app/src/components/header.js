a3a79d143790fabbedfe89ed50b9274e607e5450
fmpro12 <yank@ad.checkpoint.com>
2018-08-09 18:39:19 +0300

    addded_header

---
+++ my-app/src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+    render() {
+    return (
+    <div className="Header">
+    <i class="fab fa-twitter"></i>
+    </div>     
+    )};
+  }
+  
+  export default Header;
