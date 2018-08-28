2abd1bd5d017163390da934217803ececca04629
fmpro12 <yank@ad.checkpoint.com>
2018-08-28 19:17:06 +0300

    added_ul_rightdown

--- my-app/src/components/divs/rightdown.js
+++ my-app/src/components/divs/rightdown.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import "./rightdown.css"
+
 
 class Rightdown extends Component {
     render() {
     return (
-    <div className="rightdown"></div> 
+    <div className="rightdown">
+    <ul className="links">
+    <li class="Footer_item">© 2018 Twitter</li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">About</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Help Center</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Terms</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Privacy policy</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">About</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Cookies</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Add info</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Brand</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Blog</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Status</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Apps</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Jobs</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Add info</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Marketing</a></li>
+    <li class="Footer_item"><a class="Footer_link" href="link" rel="noopener">Businesses</a></li>
+    </ul>
+    </div> 
     )};
   }
   
