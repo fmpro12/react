adb036bfb1b518caf0ba30eec9ce096bef05aa58
fmport12 <yank@ad.checkpoint.com>
2018-08-30 18:53:03 +0300

    added_search

--- my-app/src/components/divs/main.js
+++ my-app/src/components/divs/main.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import './main.css'
 
 
 class Main extends Component {
     render() {
     return (
-    <div className="main"></div> 
+    <div className="main">
+    <div className="search_div">
+    <input type="text" className="search" placeholder="What's happening?"></input>
+    </div>
+    </div> 
     )};
   }
   
