aa6985d6468ed4753a3bceb10731b555e4d62a5a
fmport12 <yank@ad.checkpoint.com>
2018-09-02 17:01:45 +0300

    added_post777

--- my-app/src/components/feeds/feed.js
+++ my-app/src/components/feeds/feed.js
@@ -1,27 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './feed.css'
 
 
 
-class Feed extends Component {
-  constructor(props) {
-super(props);
-this.state = {welcome: "What's up?"};
-this.createPost = this.createPost.bind(this);
-  }
-createPost(){
-return React.createElement('div', {className: 'post'});
+class NewComponent extends React.Component {
+  render() {
+    return (
+      <div className="post" {...this.props}>
+        new component
+      </div>
+    );
+  }  
 }
 
-    render() {
+class Button extends React.Component {
+  render() {
     return (
-    <div className="feed">
-    <div className="search_div" tabindex="0">
-    <input type="text" className="search" placeholder="What's happening?"></input>
-    <button onClick={()=> {this.createPost()}}>Tweet</button>
-    </div>
-    </div> 
-    )};
+      <button {...this.props}>
+        click
+      </button>
+    );
+  }  
+}
+
+class Feed extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      clicked: false
+    };
+    
+    this.handleClick = this.handleClick.bind(this);
   }
   
-  export default Feed;
+  handleClick() {
+    this.setState({
+      clicked: true
+    });
+  }
+  
+  render() {
+    return (
+      <div className="feed">
+      <div className="search_div" tabindex="0">
+      <input type="text" className="search" placeholder="What's happening?"></input>
+        <Button onClick={this.handleClick} />
+        {this.state.clicked ? <NewComponent /> : null}
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Feed;
+
+
